b86b5306d978fefa2f8f8df1c5d7466a1b94c1a9
mltrev23 <trevor.dev23@gmail.com>
2024-04-17 09:21:26 -0500

    try on local images

--- main.py
+++ main.py
@@ -11,22 +11,29 @@ model.multi_label = False  # NMS multiple labels per box
 model.max_det = 1000  # maximum number of detections per image
 
 # set image
-img = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+#img = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+images = ['1.png', '1_rotated.png', '2.png', '3.png', '4.png']
 
 # perform inference
-results = model(img, size=640)
+for img in images:
+    results = model(img, size=640)
 
-# inference with test time augmentation
-results = model(img, augment=True)
+    # inference with test time augmentation
+    results = model(img, augment=True)
 
-# parse results
-predictions = results.pred[0]
-boxes = predictions[:, :4] # x1, y1, x2, y2
-scores = predictions[:, 4]
-categories = predictions[:, 5]
+    # parse results
+    predictions = results.pred[0]
+    boxes = predictions[:, :4] # x1, y1, x2, y2
+    scores = predictions[:, 4]
+    categories = predictions[:, 5]
 
-# show detection bounding boxes on image
-results.show()
+    # show detection bounding boxes on image
+    results.show()
 
-# save results into "results/" folder
-results.save(save_dir='results/')
+    # save results into "results/" folder
+    results.save()
+
+    print(predictions)
+    print(boxes)
+    print(scores)
+    print(categories)
